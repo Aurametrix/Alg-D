64dd3335a4e5bd34b6d5fc71a0222dabdfbfef61
Aurametrix <irene@aurametrix.com>
2015-12-05 16:04:05 -0800

    testing

--- PUZZLES/step_up.d
+++ PUZZLES/step_up.d
@@ -1,3 +1,17 @@
+import std.stdio;
+import std.random;
+ 
+int position;
+bool step()
+{
+    bool r = rand() > (uint.max / 2);
+    if(r)
+        writefln("Climbed up to %d", ++position);
+    else
+        writefln("Fell down to %d", --position);
+    return r;
+}
+
 void step_up()
 {
     while(!step)
@@ -7,4 +21,10 @@ void step_up()
 void step_up_nr()
 {
     for(uint i = 0; i < 1; step ? ++i : --i) {};
+}
+
+void main()
+{
+    rand_seed(0, 0); // to make it somewhat repeatable
+    step_up;
 }
